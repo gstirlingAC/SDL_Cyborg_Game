43bcf84c8c9e22c906712870e35bbd589fb7e867
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-04-29 10:06:26 +0100

    Create a time controller to control all time related processes in the game i.e. pausing, resetting etc.

---
+++ timeController.h
@@ -0,0 +1,31 @@
+#ifndef TIMECONTROLLER
+#define TIMECONTROLLER
+
+#include <iostream>
+#include "SDL.h"
+
+class TimeController
+{
+public:
+	// reference values
+	static const int PLAY_STATE, PAUSE_STATE;
+
+	int timeState;
+	Uint32 lastUpdate; // last time we checked how many ticks we were up to
+	float dT; // delta time, in seconds (1 = 1 second, 0.5 is half a second etc) - Time since the last frame was rendered onto screen
+
+	TimeController();
+	void updateTime(); // update lastUpdate and dT
+	void pause();
+	void resume();
+	void reset();
+
+	static TimeController timeController;
+
+private:
+
+};
+
+
+#endif // !TIMECONTROLLER
+
